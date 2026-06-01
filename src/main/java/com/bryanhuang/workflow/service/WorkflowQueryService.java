edcec0de005c0228d1655eb56cb07077161cb50a
Bryan Huang <bryanhuang222@gmail.com>
2026-06-01 00:13:17 -0400

    refactor queries to its own file

---
+++ src/main/java/com/bryanhuang/workflow/service/WorkflowQueryService.java
@@ -0,0 +1,52 @@
+package com.bryanhuang.workflow.service;
+
+import com.bryanhuang.workflow.entity.WorkflowEntity;
+import com.bryanhuang.workflow.entity.WorkflowExecutionEntity;
+import com.bryanhuang.workflow.exception.WorkflowNotFoundException;
+import com.bryanhuang.workflow.mapper.WorkflowEntityMapper;
+import com.bryanhuang.workflow.model.Workflow;
+import com.bryanhuang.workflow.repository.WorkflowExecutionRepository;
+import com.bryanhuang.workflow.repository.WorkflowRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class WorkflowQueryService {
+
+    private final WorkflowEntityMapper workflowEntityMapper;
+    private final WorkflowRepository workflowRepository;
+    private final WorkflowExecutionRepository workflowExecutionRepository;
+
+    public Workflow findWorkflow(UUID workflowId)  {
+        WorkflowEntity workflowEntity = getWorkflowById(workflowId);
+        return workflowEntityMapper.toWorkflow(workflowEntity);
+    }
+
+    private WorkflowEntity getWorkflowById(UUID workflowId) {
+        return workflowRepository.findById(workflowId)
+                .orElseThrow(() -> new WorkflowNotFoundException(
+                        "Workflow not found with id: " + workflowId
+                ));
+    }
+
+    // TODO: check if workflow name already exists and throw exception if it does
+    @Transactional
+    public WorkflowEntity saveWorkflowEntity(WorkflowEntity workflowEntity) {
+
+        return workflowRepository.save(workflowEntity);
+        // Ensure workflow name isn't already used in the DB
+
+    }
+
+    @Transactional
+    public WorkflowExecutionEntity saveWorkflowExecutionEntity(WorkflowExecutionEntity workflowExecutionEntity) {
+
+        return workflowExecutionRepository.save(workflowExecutionEntity);
+        // Ensure workflow name isn't already used in the DB
+
+    }
+}
