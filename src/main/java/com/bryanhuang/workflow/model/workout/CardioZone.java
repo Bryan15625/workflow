42633d461141c812c03f944bf500c72daa144ad7
Bryan Huang <bryanhuang222@gmail.com>
2026-08-01 19:03:53 -0400

    add none case for when cardio is not completed

--- src/main/java/com/bryanhuang/workflow/model/workout/CardioZone.java
+++ src/main/java/com/bryanhuang/workflow/model/workout/CardioZone.java
@@ -1,31 +1,23 @@
 package com.bryanhuang.workflow.model.workout;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public enum CardioZone {
+    NONE,
     ONE,
     TWO,
     THREE,
     FOUR,
     FIVE;
 
-    private static final Map<Integer, CardioZone> cardioZoneMap = new HashMap<>();
+    private static final CardioZone[] VALUES = CardioZone.values();
 
-    static {
-        cardioZoneMap.put(1, CardioZone.ONE);
-        cardioZoneMap.put(2, CardioZone.TWO);
-        cardioZoneMap.put(3, CardioZone.THREE);
-        cardioZoneMap.put(4, CardioZone.FOUR);
-        cardioZoneMap.put(5, CardioZone.FIVE);
-    }
-    public static CardioZone fromInt(Integer zone) {
-        if (zone == null || zone < 1 || zone > 5) {
-            throw new IllegalArgumentException("Zone cannot be null or outside of [1,5] range");
+    public static CardioZone fromInt(int zone) {
+
+        if (zone < 1 || zone >= VALUES.length) {
+            throw new IllegalArgumentException(
+                    "Cardio zone integer must be between 1 and 5. Found: " + zone
+            );
         }
-        return cardioZoneMap.get(zone);
+
+        return VALUES[zone];
     }
-
 }
-
-
