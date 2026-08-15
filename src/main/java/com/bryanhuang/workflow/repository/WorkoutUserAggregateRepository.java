d8f7caf888ef7220b92175849b6d2a63873a5656
Bryan Huang <bryanhuang222@gmail.com>
2026-08-15 09:52:54 -0400

    add find method for step 3

--- src/main/java/com/bryanhuang/workflow/repository/WorkoutUserAggregateRepository.java
+++ src/main/java/com/bryanhuang/workflow/repository/WorkoutUserAggregateRepository.java
@@ -1,6 +1,9 @@
 package com.bryanhuang.workflow.repository;
 
+import com.bryanhuang.workflow.entity.WorkflowExecutionEntity;
 import com.bryanhuang.workflow.entity.WorkoutUserAggregateEntity;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -9,6 +12,10 @@ import org.springframework.data.repository.query.Param;
 import java.util.UUID;
 
 public interface WorkoutUserAggregateRepository extends JpaRepository<WorkoutUserAggregateEntity, UUID> {
+    Slice<WorkoutUserAggregateEntity> findSliceByWorkflowExecutionEntity(
+            WorkflowExecutionEntity workflowExecutionEntity,
+            Pageable pageable
+    );
 
     @Modifying
     @Query("DELETE FROM WorkoutUserAggregateEntity w WHERE w.workflowExecutionEntity.workflowExecutionId = :workflowExecutionId")
